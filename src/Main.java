b82c00f1a5e5ae4cdea92f9fa0e876634fb3209b
Naina Noorani <nainanoorani@gmail.com>
2022-09-27 14:36:54 -0500

    part 1: cupcakeinator complete

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ArrayList<Cupcake> cupcakeMenu = new ArrayList<Cupcake>();
+        Cupcake cupcake = new Cupcake();
+        RedVelvet redVelvet = new RedVelvet();
+        Chocolate chocolate = new Chocolate();
+        System.out.println("We are in the middle of creating the cupcake menu. We currently have three cupcakes on the menu but we need to decide on pricing.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("We are deciding on the price for our standard cupcake. Here is the description:");
+        cupcake.type();
+        System.out.println("How much would you like to charge for the cupcake? (Input a numerical number taken to 2 decimal places)");
+        String priceText = input.nextLine();
+        double price = Double.parseDouble(priceText);
+        cupcake.setPrice(price);
+        //red velvet
+        System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description:");
+        redVelvet.type();
+        System.out.println("How much would you like to charge for the cupcake? (Input a numerical number taken to 2 decimal places)");
+        String rvPriceText = input.nextLine();
+        double rvPrice = Double.parseDouble(rvPriceText);
+        redVelvet.setPrice(rvPrice);
+        //chocolate
+        System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description:");
+        chocolate.type();
+        System.out.println("How much would you like to charge for the cupcake? (Input a numerical number taken to 2 decimal places)");
+        String cPriceText = input.nextLine();
+        double cPrice = Double.parseDouble(cPriceText);
+        chocolate.setPrice(cPrice);
+         //add to array list
+        cupcakeMenu.add(cupcake);
+        cupcakeMenu.add(redVelvet);
+        cupcakeMenu.add(chocolate);
     }
 }
 
@@ -19,5 +52,13 @@ class Cupcake{
 }
 
 class RedVelvet extends Cupcake{
+    public void type(){
+        System.out.println("A red velvet based cupcake, with cream cheese frosting.");
+    }
+}
 
+class Chocolate extends Cupcake{
+    public void type(){
+        System.out.println("A chocolate based cupcake, with chocolate frosting.");
+    }
 }
